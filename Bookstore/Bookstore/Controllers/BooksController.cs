c32bb57939afab3a308132ae703a4e26ddb6011f
Akram-Boktor <35446384+AkramBoktor@users.noreply.github.com>
2019-08-10 15:36:46 +0200

    Add files via upload

---
+++ Bookstore/Bookstore/Controllers/BooksController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Helpers;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class BooksController : ODataController
+    {
+        private BookStoreContext _db;
+
+        public BooksController(BookStoreContext context)
+        {
+            _db = context;
+            if (context.Books.Count() == 0)
+            {
+                foreach (var b in DataSource.GetBooks())
+                {
+                    context.Books.Add(b);
+
+                }
+                context.SaveChanges();
+            }
+        }
+
+
+        [EnableQuery]
+       // [ODataRoute("Books")]
+        //http://localhost:8288/odata/Books?$select=id,ISBN
+        public IEnumerable<Book> Get(ODataQueryOptions<Book> options)
+        {
+
+            var queryBuilder = new SqlQueryBuilder(options);
+            var sql = queryBuilder.ToSql();
+            //sql select id from book orderby id
+            return _db.Books;
+        }
+
+
+       
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook([FromBody] Book book)
+        {
+         
+            _db.Books.Add(book);
+            await _db.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ Bookstore/Bookstore/Models/DataSource.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public static class DataSource
+    {
+        private static IList<Book> _books { get; set; }
+
+        public static IList<Book> GetBooks()
+        {
+            if (_books != null)
+            {
+                return _books;
+            }
+
+            _books = new List<Book>();
+
+            // book #1
+            Book book = new Book
+            {
+                Id = 1,
+                ISBN = "978-0-321-87758-1",
+                Title = "Essential C#5.0",
+                Author = "Mark Michaelis",
+                Price = 59.99m,
+             
+            };
+            _books.Add(book);
+
+            // book #2
+            book = new Book
+            {
+                Id = 2,
+                ISBN = "063-6-920-02371-5",
+                Title = "Enterprise Games",
+                Author = "Michael Hugos",
+                Price = 49.99m,
+                
+            };
+            _books.Add(book);
+
+            return _books;
+        }
+    }
+
+}
